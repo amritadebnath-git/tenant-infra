fdd1aa1a512a1a21a031f4848cf9f8417acad3b2
amritadebnath-git <amritadebnathcloud@gmail.com>
2025-08-03 11:53:08 +0530

    Docker github pipeline and dynamic unittest

--- app/Test_app.py
+++ app/Test_app.py
@@ -1,5 +1,6 @@
 import unittest
-from app import app
+import uuid
+from app.app import app
 
 class FlaskTestCase(unittest.TestCase):
 
@@ -12,11 +13,14 @@ class FlaskTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_register_user(self):
+        random_username = f"user_{uuid.uuid4().hex[:6]}"
+        random_password = f"pass_{uuid.uuid4().hex[:6]}"
         response = self.client.post("/register", data={
-            "username": "testuser04",
-            "password": "testpass04"
+            "username": random_username,
+            "password": random_password
         }, follow_redirects=True)
-        print(response.data);
+
+        print(f"Tested with username: {random_username}, password: {random_password}")
         self.assertIn(b"Account created! Please login.", response.data)
 
 if __name__ == "__main__":
